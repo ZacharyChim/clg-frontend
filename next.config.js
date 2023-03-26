424d7f8fcd8050b8a5bab7d3fde2f41cb7d3306f
Zack <zecharyw@gmail.com>
2023-03-27 01:05:02 +0800

    Updated the IP to localhost

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const dns = require('dns')
 dns.setDefaultResultOrder('ipv4first')
 const nextConfig = {
   images: {
-    domains: ['165.22.255.85'],
+    domains: ['localhost'],
     unoptimized: true,
   },
   reactStrictMode: true,
@@ -17,9 +17,9 @@ const nextConfig = {
     // For Docker Containers to communicate
     // NEXT_PUBLIC_STRAPI_API_URL: 'http://backend:1338/api',
     // For local development without Docker
-    NEXT_PUBLIC_STRAPI_API_URL: 'http://165.22.255.85:1338/api',
-    NEXT_PUBLIC_STRAPI_URL: 'http://165.22.255.85:1338',
-    NEXT_PUBLIC_SITE_URL: 'http://165.22.255.85:3000',
+    NEXT_PUBLIC_STRAPI_API_URL: 'http://localhost:1338/api',
+    NEXT_PUBLIC_STRAPI_URL: 'http://localhost:1338',
+    NEXT_PUBLIC_SITE_URL: 'http://localhost:3000',
   },
 }
 
