b749a6f451cc7763aed3920f087eec0f29453ac0
Zack <zecharyw@gmail.com>
2023-03-27 03:37:42 +0800

    Updated Strapi URL

--- next.config.js
+++ next.config.js
@@ -17,8 +17,8 @@ const nextConfig = {
     // For Docker Containers to communicate
     // NEXT_PUBLIC_STRAPI_API_URL: 'http://backend:1338/api',
     // For local development without Docker
-    NEXT_PUBLIC_STRAPI_API_URL: 'http://localhost:1338/api',
-    NEXT_PUBLIC_STRAPI_URL: 'http://localhost:1338',
+    NEXT_PUBLIC_STRAPI_API_URL: 'http://165.22.255.85:1338/api',
+    NEXT_PUBLIC_STRAPI_URL: 'http://165.22.255.85:1338',
     NEXT_PUBLIC_SITE_URL: 'http://localhost:3000',
   },
   typescript: {
