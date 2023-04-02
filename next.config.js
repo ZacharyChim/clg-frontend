42a7cda46b1bdc9d28dc13ca0716cce8f347544c
Zack <zecharyw@gmail.com>
2023-04-02 18:28:19 +0800

    Update config for production

--- next.config.js
+++ next.config.js
@@ -4,13 +4,13 @@ const dns = require('dns')
 dns.setDefaultResultOrder('ipv4first')
 const nextConfig = {
   images: {
-    domains: ['localhost'],
-    // remotePatterns: [
-    //   {
-    //     protocol: 'http',
-    //     hostname: '**',
-    //   },
-    // ],
+    // domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+    ],
     // loader: 'imgix',
     // path: '/',
     unoptimized: true,
@@ -25,9 +25,9 @@ const nextConfig = {
     // For Docker Containers to communicate
     // NEXT_PUBLIC_STRAPI_API_URL: 'http://backend:1338/api',
     // For local development without Docker
-    // NEXT_PUBLIC_STRAPI_API_URL: 'http://165.22.255.85:1338/api',
-    // NEXT_PUBLIC_STRAPI_URL: 'http://165.22.255.85:1338',
-    // NEXT_PUBLIC_SITE_URL: 'https://clg-frontend.vercel.app',
+    NEXT_PUBLIC_STRAPI_API_URL: 'http://202.181.146.26:1338/api',
+    NEXT_PUBLIC_STRAPI_URL: 'http://202.181.146.26:1338',
+    NEXT_PUBLIC_SITE_URL: 'https://clg-frontend.vercel.app',
     // NEXT_PUBLIC_SITE_URL: 'http://localhost:3000',
   },
   typescript: {
