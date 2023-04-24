8ef11d426d85a6c13104073f86aac169278ea6ab
Zack <zecharyw@gmail.com>
2023-04-24 12:44:46 +0800

    Dumbchat testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import Header from './Header'
 import Footer from './Footer'
 import '../styles/globals.css'
+import Script from 'next/script'
 
 export default function RootLayout({
   // Layouts must accept a children prop.
@@ -22,6 +23,8 @@ export default function RootLayout({
         {children}
         {/* @ts-ignore */}
         <Footer />
+        <Script src='https://chatroom-v2.dumbchat.ai/lib/TC.chatbot.umd.prod.min.js'></Script>
+        <Script>new TC_ChatBot('CP303284@CB87358406')</Script>
       </body>
     </html>
   )
