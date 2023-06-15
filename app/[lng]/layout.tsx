0eaee5b147cdfa05a62a1af6c7668f32f63aa9a0
Zack <zecharyw@gmail.com>
2023-06-15 17:10:43 +0800

    Moved Dumchat script from HEAD to BODY

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -25,8 +25,6 @@ export default function RootLayout({
     <html lang={lng} dir={dir(lng)}>
       <head>
         <title>CLG Group</title>
-        <Script src='https://chatroom.dumbchat.ai/lib/TC.chatbot.umd.prod.min.js?v=20230425' />
-        <Script src='chatbot.js' />
       </head>
       <body>
         {/* @ts-ignore */}
@@ -35,6 +33,8 @@ export default function RootLayout({
         {children}
         {/* @ts-ignore */}
         <Footer lng={lng} />
+        <Script src='https://chatroom.dumbchat.ai/lib/TC.chatbot.umd.prod.min.js?v=20230425' />
+        <Script src={process.env.NEXT_PUBLIC_SITE_URL + '/chatbot.js'} />
       </body>
     </html>
   )
